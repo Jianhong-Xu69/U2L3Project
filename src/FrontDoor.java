ae83eda9bb7d6bf0d0743d8f66d0054fb3336ef7
BT_4N2_18 <jxu5406@bths.edu>
2022-09-30 12:17:57 -0400

    Patchnotes v1.03
    Added ChatBot class
    greeting() prints predetermined greeting message
    help() prints predetermined help message
    weather() prints predetermined weather message
    distanceInfo() prints predetermined distanceInfo message
    feetToMeters() takes a double input into the parameter, and returns a double
    goodbye() prints a predetermined goodbye message
    favoriteNumber() takes an int input into a parameter, and prints a custom message
    feetToMeters() takes a double input into a parameter, and returns a double
    
    Added code in FrontDoor to run ChatBot
    Uses selection to perform different methods available in ChatBot
    Has a result for invalid inputs

--- src/FrontDoor.java
+++ src/FrontDoor.java
@@ -1,6 +1,30 @@
-import java.util.Scanner; // holdover from when I needed it
+import java.util.Scanner;
 public class FrontDoor {
     public static void main(String[] args) { // code goes here
-
+        Scanner input = new Scanner(System.in);
+        ChatBot jamie = new ChatBot();
+        jamie.greeting();
+        System.out.println("What would you like to do (w for weather, i for info, d for distance, n for favorite number, c for converting feet to meters)");
+        String choice = input.nextLine();
+        if (choice.equals("w")) {
+            jamie.weather();
+        } else if (choice.equals("i"))
+        {
+            jamie.help();
+        } else if (choice.equals("d"))
+        {
+            jamie.distanceInfo();
+        } else if (choice.equals("n")) {
+            System.out.println("What is your favorite number?");
+            jamie.favoriteNumber(input.nextInt());
+        } else if (choice.equals("c"))
+        {
+            System.out.println("What much are you converting?");
+            jamie.feetToMeters(input.nextDouble());
+        }else
+        {
+            System.out.println("That's not a valid input");
+        }
+        jamie.goodbye();
     }
 }
